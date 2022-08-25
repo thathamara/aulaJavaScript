3615b3f76fc8a4b7cea3bc856bf85b5198af731f
Thamara Leany Machado <106841849+thathamara@users.noreply.github.com>
2022-08-25 14:05:34 -0300

    Add files via upload

---
+++ aula1/js1.js
@@ -0,0 +1,10 @@
+/*{
+    let num1=10;
+    let num2=5;
+    let soma=num1+num2;
+    console.log(soma);
+}*/
+{
+    let num1=prompt('digite um numero:');
+    alert('O numero digitado foi:'+num1);
+}
