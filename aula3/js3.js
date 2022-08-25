69ef2cce909794e307d2b5402555970418df366f
Thamara Leany Machado <106841849+thathamara@users.noreply.github.com>
2022-08-25 15:09:31 -0300

    Add files via upload

---
+++ aula3/js3.js
@@ -0,0 +1,12 @@
+{
+    let str='Manipulando strings com JS';
+    alert(str.toLowerCase());
+
+    console.log(str.slice(0,5));
+
+    alert(str.length);
+
+    let str2='Vamos concatenar as duas strings';
+
+    console.log(`${str} ${str2}`);
+}
