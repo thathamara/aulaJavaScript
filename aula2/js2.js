2cde69b594fa7545a714c6489124cd95d71dfb45
Thamara Leany Machado <106841849+thathamara@users.noreply.github.com>
2022-08-25 14:52:44 -0300

    Add files via upload

---
+++ aula2/js2.js
@@ -0,0 +1,23 @@
+{
+    let num1=prompt('Digite o numero total de figurinhas:');
+    let unicas=num1-15;
+    console.log(`Marcia ficou com um total de: ${unicas}`);
+}
+{
+    let numLatas=prompt('Digite o numero de latinhas:');
+    let divisao=numLatas/4;
+    console.log(`Em cada caixa cabe: ${divisao}`);
+}
+{
+    let num=10;
+    num-=1;
+    console.log(num);
+    num-=2;
+    console.log(num);
+    num-=2;
+    console.log(num);
+    num-=2;
+    console.log(num);
+    num-=2;
+    console.log(num);
+}
